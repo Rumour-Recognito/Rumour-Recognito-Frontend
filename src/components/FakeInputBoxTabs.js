f74831f330251af1280b52cfbd7c39acce350855
sosaha <Vnjh@3976>
2022-06-05 23:12:41 +0530

    console.log removed

--- src/components/FakeInputBoxTabs.js
+++ src/components/FakeInputBoxTabs.js
@@ -107,7 +107,6 @@ class FakeInputBoxTabs extends React.Component {
 
   handleSubmitRefresh = (event) => {
     event.preventDefault()
-    console.log('test')
   }
 
   componentDidMount() {
@@ -327,8 +326,6 @@ class FakeInputBoxTabs extends React.Component {
 
   //control the Input-box
   handleSearchInput = (event) => {
-    console.log('changing')
-
     if (this.state.tabValue == 3) {
       var newSearchInputs = [...this.state.searchInputs]
       newSearchInputs[this.state.tabValue] = event.target.value
@@ -407,8 +404,6 @@ class FakeInputBoxTabs extends React.Component {
       jobId: newJobIds
     })
 
-    console.log('searchInput : ', this.state.searchInputs[tab])
-
     if (tab === 0) {
       //analyse facebook post
       verdict = await this.analyseFacebookPost(postText, jobId)
@@ -444,8 +439,6 @@ class FakeInputBoxTabs extends React.Component {
   }
 
   handleTabChange = (event, newValue) => {
-    console.log(this.state.searchInputs)
-
     this.setState({
       tabValue: newValue
     })
@@ -662,13 +655,6 @@ const Input = styled('input')({
 class UploadButton extends React.Component {
   constructor(props) {
     super(props)
-
-    console.log(this.props.value)
-  }
-
-  componentDidMount() {
-    console.log('test')
-    console.log(this.props.value)
   }
 
   render() {
